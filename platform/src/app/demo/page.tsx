1bdd4c5ace6a7e50588dbe45c504f7d707d9f773
azapg <allangameyt28@gmail.com>
2025-08-20 17:46:21 -0500

    feat: update Skeleton component widths for improved layout consistency

--- platform/src/app/demo/page.tsx
+++ platform/src/app/demo/page.tsx
@@ -95,15 +95,15 @@ export default function CoursesPage() {
         <Card className={"bg-gray-50 cursor-wait flex flex-col h-full"}>
           <CardHeader className={"flex-1 flex flex-col items-start"}>
             <CardTitle>
-              <Skeleton className="h-[20px] w-[300px] rounded-full bg-gray-200" />
+              <Skeleton className="h-[20px] w-25 rounded-full bg-gray-200" />
             </CardTitle>
             <CardDescription className={"space-y-2 mt-2"}>
-              <Skeleton className="h-4 w-[250px] bg-gray-200" />
-              <Skeleton className="h-4 w-[200px] bg-gray-200" />
+              <Skeleton className="h-4 w-45 bg-gray-200" />
+              <Skeleton className="h-4 w-35 bg-gray-200" />
             </CardDescription>
           </CardHeader>
           <CardFooter>
-            <Skeleton className="h-4 w-[150px] bg-gray-200" />
+            <Skeleton className="h-4 w-20 bg-gray-200" />
           </CardFooter>
         </Card>
       </div>
