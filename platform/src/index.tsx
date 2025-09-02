fb8f86002464ae5ccea785aacab1ae609c86dab1
azapg <allangameyt28@gmail.com>
2025-09-02 12:03:56 -0500

    feat: add login and register pages with routing support

--- platform/src/index.tsx
+++ platform/src/index.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from "./home";
 import CoursesPage from "@/app/demo/courses/page";
+import LoginPage from "@/app/login/page";
+import RegisterPage from "@/app/register/page";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path={"demo"} element={<CoursesPage />} />
+      <Route path={"login"} element={<LoginPage />} />
+      <Route path={"register"} element={<RegisterPage />} />
       <Route path="*" element={<div>Not Found</div>} />
     </Routes>
   )
