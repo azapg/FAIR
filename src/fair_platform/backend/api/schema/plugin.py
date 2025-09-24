3f2d185e2101a4cf98acd940140f55cd3a48a6d3
azapg <allangameyt28@gmail.com>
2025-09-24 18:27:46 -0500

    feat: make PluginBase immutable

--- src/fair_platform/backend/api/schema/plugin.py
+++ src/fair_platform/backend/api/schema/plugin.py
@@ -1,4 +1,4 @@
-from typing import Optional, Dict, Any
+from typing import Optional
 
 from pydantic import BaseModel
 
@@ -11,38 +11,13 @@ class PluginBase(BaseModel):
     version: str = None
     hash: str
     source: str
-    settings: Optional[Dict[str, Any]] = None
 
     class Config:
         from_attributes = True
         alias_generator = lambda field_name: ''.join(word.capitalize() if i > 0 else word for i, word in enumerate(field_name.split('_')))
         validate_by_name = True
-
-class PluginCreate(PluginBase):
-    pass
-
-
-class PluginUpdate(BaseModel):
-    name: Optional[str] = None
-    author: Optional[str] = None
-    version: Optional[str] = None
-    source: Optional[str] = None
-    settings: Optional[Dict[str, Any]] = None
-
-    class Config:
-        from_attributes = True
-        alias_generator = lambda field_name: ''.join(word.capitalize() if i > 0 else word for i, word in enumerate(field_name.split('_')))
-        validate_by_name = True
-
-
-class PluginRead(PluginBase):
-    pass
 
 
 __all__ = [
     "PluginBase",
-    "PluginCreate",
-    "PluginUpdate",
-    "PluginRead",
 ]
-
