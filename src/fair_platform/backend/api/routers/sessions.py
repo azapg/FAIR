89df44eac799e510193184e96ebd81f014b30b3c
azapg <allangameyt28@gmail.com>
2025-10-06 20:55:43 -0500

    feat(backend): Update workflows, plugins and sessions models/schemas for more robust and usable API

--- src/fair_platform/backend/api/routers/sessions.py
+++ src/fair_platform/backend/api/routers/sessions.py
@@ -1,19 +1,23 @@
-from typing import List
+from typing import List, Optional
 
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from uuid import UUID
 
+from sqlalchemy.orm import Session
 from starlette.websockets import WebSocket
 
 from fair_platform.backend.api.routers.auth import get_current_user
+from fair_platform.backend.api.schema.workflow import WorkflowBase
+from fair_platform.backend.data.database import session_dependency
 from fair_platform.backend.data.models import User
 
 router = APIRouter()
 
 
 class SessionCreateRequest(BaseModel):
-    workflow_id: UUID
+    workflow_id: Optional[UUID] = None
+    workflow_draft = Optional[WorkflowBase] = False
     submission_ids: List[UUID]
 
 
@@ -24,9 +28,26 @@ class SessionResponse(BaseModel):
 
 
 @router.post("/")
-def create_session(payload: SessionCreateRequest, user: User = Depends(get_current_user)):
-    raise HTTPException(status_code=501, detail="Not implemented")
+def create_session(payload: SessionCreateRequest, user: User = Depends(get_current_user), db: Session = Depends(session_dependency)):
+    if payload.workflow_id:
+        raise HTTPException(status_code=501, detail="Creating a session from an existing workflow is not implemented")
 
+    if not payload.workflow_draft:
+        raise HTTPException(status_code=400, detail="Creating a session from a non-draft workflow is not implemented")
+
+    if not payload.submission_ids:
+        raise HTTPException(status_code=400, detail="At least one submission ID must be provided")
+
+    workflow = payload.workflow_draft
+
+    # TODO: From here, we need to:
+    #   1. Create some sort of task that will manage the session lifecycle
+    #   2. Create a session entry in the database
+    #   3. Make that task start processing the submissions with the workflow. It should go through
+    #      plugins step by step, running each plugin on all submissions before moving to the next plugin.
+    #   4. The websocket should be able to manage and retrieve the status of the session and stream logs.
+
+    return None
 
 # ws://HOST/api/sessions/{session_id}
 @router.websocket("{session_id}")
--- src/fair_platform/backend/data/models/plugin.py
+++ src/fair_platform/backend/data/models/plugin.py
@@ -1,6 +1,6 @@
 from sqlalchemy import String, Text, JSON, PrimaryKeyConstraint
 from sqlalchemy.orm import Mapped, mapped_column
-from typing import Optional
+from typing import Optional, Dict, Any
 
 from ..database import Base
 
@@ -11,11 +11,13 @@ class Plugin(Base):
 
     id: Mapped[str] = mapped_column(String, nullable=False)
     name: Mapped[str] = mapped_column(String, nullable=False)
-    author: Mapped[Optional[str]] = mapped_column(String, nullable=True)
-    version: Mapped[Optional[str]] = mapped_column(String, nullable=True)
+    author: Mapped[Optional[str]] = mapped_column(String, nullable=False)
+    version: Mapped[Optional[str]] = mapped_column(String, nullable=False)
     hash: Mapped[str] = mapped_column(String, nullable=False)
     source: Mapped[str] = mapped_column(Text, nullable=False)
     meta: Mapped[Optional[dict]] = mapped_column(JSON, nullable=True)
+    type: Mapped[str] = mapped_column(String, nullable=False)
+    settings_schema: Mapped[Optional[Dict[str, Any]]] = mapped_column(JSON, nullable=True)
 
     def __repr__(self) -> str:
         return f"<Plugin id={self.id} hash={self.hash} name={self.name} source={self.source}>"
