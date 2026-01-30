1dfe1a7a69b393888322fd38ee23ec6264ca64df
azapg <allangameyt28@gmail.com>
2026-01-29 22:21:54 -0500

    feat: stop showing sidebar at login and register pages

--- frontend-dev/src/main.tsx
+++ frontend-dev/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import { Providers } from "./providers";
 import { App } from "./index";
 
@@ -10,26 +10,46 @@ import { AppSidebar } from "@/components/app-sidebar";
 import { Toaster } from "sonner";
 import "@/i18n";
 
+function MainLayout() {
+  const location = useLocation();
+  const isAuthPage = ["/login", "/register"].includes(location.pathname);
+
+  if (isAuthPage) {
+    return (
+      <div className="min-h-svh w-full">
+        <Toaster richColors position="bottom-left" />
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
+      </div>
+    );
+  }
+
+  return (
+    <SidebarProvider
+      className="flex min-h-svh w-full flex-col"
+      cookieName="app_sidebar_state"
+      keyboardShortcut="g"
+      width="16rem"
+    >
+      <div className="h-full flex">
+        <AppSidebar />
+        <SidebarInset>
+          <Toaster richColors position="bottom-left" />
+          <ErrorBoundary>
+            <App />
+          </ErrorBoundary>
+        </SidebarInset>
+      </div>
+    </SidebarProvider>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Providers>
       <BrowserRouter>
-        <SidebarProvider
-          className={"flex min-h-svh w-full flex-col"}
-          cookieName="app_sidebar_state"
-          keyboardShortcut="g"
-          width="16rem"
-        >
-          <div className={"h-full flex"}>
-            <AppSidebar />
-            <SidebarInset>
-              <Toaster richColors position="bottom-left" />
-              <ErrorBoundary>
-                <App />
-              </ErrorBoundary>
-            </SidebarInset>
-          </div>
-        </SidebarProvider>
+        <MainLayout />
       </BrowserRouter>
     </Providers>
   </React.StrictMode>,
