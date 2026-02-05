75a47451ee470245391674adfdcb00a903d7b8d1
azapg <allangameyt28@gmail.com>
2026-02-05 15:13:13 -0500

    feat: remove two-column attachment sections from submission sheet

--- frontend-dev/src/app/assignment/components/submissions/submission-sheet.tsx
+++ frontend-dev/src/app/assignment/components/submissions/submission-sheet.tsx
@@ -45,72 +45,66 @@ export function SubmissionSheet({
   return (
     <Sheet open={open} onOpenChange={onOpenChange}>
       <SheetContent
-        className="w-[1000px] sm:max-w-[1000px]"
+        className="w-4/5 md:w-1/3"
         onOpenAutoFocus={(e) => e.preventDefault()}
       >
-        <div className="flex flex-row h-full">
-          <div className="w-2/3">
-            <SheetHeader>
-              <SheetTitle className="text-4xl">
-                {submission.submitter?.name}
-              </SheetTitle>
-            </SheetHeader>
-            <div className="grid flex-1 auto-rows-min gap-6 px-4">
-              <PropertiesDisplay scroll gapX={2.5}>
-                <Property>
-                  <PropertyLabel>Status</PropertyLabel>
-                  <PropertyValue>
-                    <SubmissionStatusLabel status={submission.status} />
-                  </PropertyValue>
-                </Property>
+        <SheetHeader>
+          <SheetTitle className="text-4xl">
+            {submission.submitter?.name}
+          </SheetTitle>
+        </SheetHeader>
+        <div className="grid flex-1 auto-rows-min gap-6 px-4">
+          <PropertiesDisplay scroll gapX={2.5}>
+            <Property>
+              <PropertyLabel>Status</PropertyLabel>
+              <PropertyValue>
+                <SubmissionStatusLabel status={submission.status} />
+              </PropertyValue>
+            </Property>
 
-                <Property>
-                  <PropertyLabel>Grade</PropertyLabel>
-                  <PropertyValue>
-                    <InlineEditableScore submission={submission} />
-                  </PropertyValue>
-                </Property>
+            <Property>
+              <PropertyLabel>Grade</PropertyLabel>
+              <PropertyValue>
+                <InlineEditableScore submission={submission} />
+              </PropertyValue>
+            </Property>
 
-                <Property>
-                  <PropertyLabel>Feedback</PropertyLabel>
-                  <PropertyValue>
-                    <InlineEditableFeedback
-                      submission={submission}
-                      startInEditMode={focusOn === "feedback"}
-                    />
-                  </PropertyValue>
-                </Property>
+            <Property>
+              <PropertyLabel>Feedback</PropertyLabel>
+              <PropertyValue>
+                <InlineEditableFeedback
+                  submission={submission}
+                  startInEditMode={focusOn === "feedback"}
+                />
+              </PropertyValue>
+            </Property>
 
-                <Property>
-                  <PropertyLabel>Turned in</PropertyLabel>
-                  <PropertyValue>
-                    {formatShortDate(
-                      new Date(submission.submittedAt),
-                      i18n.language,
-                    )}
-                  </PropertyValue>
-                </Property>
-              </PropertiesDisplay>
-            </div>
-          </div>
-          <div className="w-1/3">
-            <h1>Attachments</h1>
-            <div className="flex flex-row gap-1 items-center">
-              {submission.artifacts && submission.artifacts.length > 0 ? (
-                submission.artifacts.map((artifact) => {
-                  const Icon = getIconForMime(artifact.mime);
-                  return (
-                    <Button key={artifact.id} variant={"secondary"} size={"sm"}>
-                      <Icon />
-                      {artifact.title}
-                      <ArrowUpRight className="text-muted-foreground" />
-                    </Button>
-                  );
-                })
-              ) : (
-                <></>
-              )}
-            </div>
+            <Property>
+              <PropertyLabel>Turned in</PropertyLabel>
+              <PropertyValue>
+                {formatShortDate(
+                  new Date(submission.submittedAt),
+                  i18n.language,
+                )}
+              </PropertyValue>
+            </Property>
+          </PropertiesDisplay>
+          <h1>Attachments</h1>
+          <div className="flex flex-row gap-1 items-center">
+            {submission.artifacts && submission.artifacts.length > 0 ? (
+              submission.artifacts.map((artifact) => {
+                const Icon = getIconForMime(artifact.mime);
+                return (
+                  <Button key={artifact.id} variant={"secondary"} size={"sm"}>
+                    <Icon />
+                    {artifact.title}
+                    <ArrowUpRight className="text-muted-foreground" />
+                  </Button>
+                );
+              })
+            ) : (
+              <></>
+            )}
           </div>
         </div>
       </SheetContent>
